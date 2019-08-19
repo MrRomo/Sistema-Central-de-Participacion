c8e6f1e7c4ce1ca3a64191cd4aa8b660633b8680
Ricardo Romo <jricardoromo21@gmail.com>
2019-08-19 10:44:07 -0500

    modal dialog center para alerta de formulario

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-String.prototype.capitalize = function() {
-    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+String.prototype.capitalize = function () {
+    return this.replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
 };
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -29,36 +29,35 @@ const formData = {
 
 async function guardar() {
     $('#guardar')[0].innerHTML = ' <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>  Guardando'
-    
-    formFields.forEach(x =>{
+
+
+    formFields.forEach(x => {
         formData[x] = ($(`#${x}`).val() == "") ? " " : $(`#${x}`).val()
     })
-
-
-    if ((formData['firstname'] == "")||(formData['lastname']  == "")||(formData['code']  == "")){
+    if ((formData['firstname'] == " ") || (formData['lastname'] == " ") || (formData['code'] == " ")) {
         errorGuardando()
         return 0
     }
     formData['firstname'] = formData['firstname'].capitalize()
     formData['lastname'] = formData['lastname'].capitalize()
     console.log(formData);
-    
+
     await db.collection("Person").doc($('#code').val()).set(formData)
-    .then(function() {
-        console.log("Document written with ID: ")
-        console.log("primero")        
-    })
-    .catch(function(error) {
-        errorGuardando()
-        console.error("Error adding document: ", error)
-    })
-    
-    $('#guardar')[0].innerHTML = 'Guardar'  
-    
+        .then(function () {
+            console.log("Document written with ID: ")
+            console.log("primero")
+        })
+        .catch(function (error) {
+            errorGuardando()
+            console.error("Error adding document: ", error)
+        })
+
+    $('#guardar')[0].innerHTML = 'Guardar'
+
 }
 
 var options = {
-    valueNames: [ 'id', 'name','code' ]
+    valueNames: ['id', 'name', 'code']
 };
 
 // Init list
@@ -67,24 +66,27 @@ var personList = new List('personList', options);
 // Sets callbacks to the buttons in the list
 var checkBtns = $('.check')
 
-async function getPerson(){
+async function getPerson() {
     const Person = await person.get()
     await Person.forEach(doc => {
         data = doc.data()
-        personList.add({ id: data.code, name: `${data.firstname} ${data.lastname}`  , code: data.code });
+        personList.add({ id: data.code, name: `${data.firstname} ${data.lastname}`, code: data.code });
     });
-   
+
 }
 
 getPerson()
 
+
+
 function errorGuardando() {
+    $('#alertModal').modal('toggle')
     $('#guardar')[0].innerHTML = '<i class="fas fa-times"></i>  Error guardando'
     $('#guardar').toggleClass("btn-danger")
-    setTimeout(()=>{
-        $('#guardar')[0].innerHTML = 'Guardar'  
+    setTimeout(() => {
+        $('#guardar')[0].innerHTML = 'Guardar'
         $('#guardar').toggleClass("btn-danger")
-    },2000)
+    }, 2000)
 }
 
 refreshCallbacks();
@@ -93,13 +95,13 @@ refreshCallbacks();
 function refreshCallbacks() {
     // Needed to add new buttons to jQuery-extended object
     checkBtns = $(checkBtns.selector);
-  
-    checkBtns.click(function() {
+
+    checkBtns.click(function () {
         var itemId = $(this).closest('tr').find('.id').text();
         var itemValues = personList.get('id', itemId)[0].values();
         console.log(itemId);
         console.log(itemValues);
-        
+
     });
 }
 
@@ -107,4 +109,3 @@ function check() {
     console.log(this.data);
     console.log("CHECKING");
 }
-  
