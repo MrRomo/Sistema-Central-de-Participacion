7377a53e7561aabdf233948cddb969efd747b5ed
Ricardo Romo <jricardoromo21@gmail.com>
2019-09-06 17:44:55 -0500

    Lista de personas

--- scripts/personas-list.js
+++ scripts/personas-list.js
@@ -1,3 +1,6 @@
+String.prototype.capitalize = function () {
+    return this.replace(/(?:^|\s)\S/g, function (a) { return a.toUpperCase(); });
+};
 
 var options = {
     valueNames: ['id', 'name', 'code', 'carreer', 'type', 'membership', 'cell', 'email', 'assistant']
@@ -5,34 +8,27 @@ var options = {
 
 var personList = new List('personList', options);
 personList.clear()
-personList.add({ id: '21wassd', name: `Ricardo ramos`, code: 21312342, carreer: 'Ingenieria Electronica',
-                type: 'Voluntario', membership: 'N/A', cell:300221754, email: 'jricardoromo21@gmail.com', assistant: 215 });
-personList.add({ id: '21wsdsd', name: `Juana ramos`, code: 21312122, carreer: 'Ingenieria Sistemas',
-                type: 'Miembro', membership: '123213', cell:300221754, email: 'jasjas@gmail.com', assistant: 544});
+personList.add({
+    id: '21wassd', name: `Ricardo ramos`, code: 21312342, carreer: 'Ingenieria Electronica',
+    type: 'Voluntario', membership: 'N/A', cell: 300221754, email: 'jricardoromo21@gmail.com', assistant: 215
+});
+personList.add({
+    id: '21wsdsd', name: `Juana ramos`, code: 21312122, carreer: 'Ingenieria Sistemas',
+    type: 'Miembro', membership: '123213', cell: 300221754, email: 'jasjas@gmail.com', assistant: 544
+});
 
-$('#personList').click(async e=>{
-    className = e.target.className.split(' ')
+async function getPerson() {
+    const Person = await person.get()
+    personList.clear()
+    await Person.forEach(doc => {
+        data = doc.data()
+        personList.add({
+            id: data.code, name: `${data.firstname} ${data.lastname}`.capitalize(), code: data.code,
+            carreer: data.carreer, type: data.type, membership: data.membership,
+            cell: data.cell, email: data.email, assistant: data.assistant
+        });
 
-    console.log(className);
-    if(!(className.indexOf('checkBtn')<0)){
-        
-        let parent = $(e.target).parents('tr')
-        let child = $(parent).children()
-        let code = parseInt(child[0].innerText)
-    
-        let formAssistant = {
-            idPerson: code,
-            idMeeting: meetingId,
-            createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
-            updatedAt: firebase.firestore.Timestamp.fromDate(new Date())
-        }
-        console.log('Register:', formAssistant);
-        const meetingData = await meeting.get()
-        noAssistant = meetingData.data().assistant
+    })
+}
 
-        await assistant.doc().set(formAssistant)        
-        await meeting.update({ assistant: noAssistant+1 })
-        getPerson()    
-    }
-    
-})
+getPerson()
