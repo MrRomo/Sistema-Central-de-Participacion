01f7b4baf4e89d5da5ccfa7dad49fd891caf4cbe
Ricardo Romo <jricardoromo21@gmail.com>
2019-09-20 00:03:53 -0500

    Formulario registro added 3

---
+++ extras/formulario/lista.js
@@ -0,0 +1,34 @@
+
+var options = {
+    valueNames: ['id', 'name', 'code']
+}
+
+var personList = new List('personList', options);
+
+
+$('#personList').click(async e=>{
+    className = e.target.className.split(' ')
+
+    console.log(className);
+    if(!(className.indexOf('checkBtn')<0)){
+        
+        let parent = $(e.target).parents('tr')
+        let child = $(parent).children()
+        let code = parseInt(child[0].innerText)
+    
+        let formAssistant = {
+            idPerson: code,
+            idMeeting: meetingId,
+            createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+            updatedAt: firebase.firestore.Timestamp.fromDate(new Date())
+        }
+        console.log('Register:', formAssistant);
+        const meetingData = await meeting.get()
+        noAssistant = meetingData.data().assistant
+
+        await assistant.doc().set(formAssistant)        
+        await meeting.update({ assistant: noAssistant+1 })
+        getPerson()    
+    }
+    
+})
---
+++ extras/formulario/script.js
@@ -0,0 +1,70 @@
+async function registrarAsistente() {
+    $('#guardar')[0].innerHTML = ' <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>  Guardando'
+    var checked = $('#interest')[0].checked
+    console.log(checked)
+
+    let formData = {
+        firstname: $('#firstname').val(),
+        lastname: $('#lastname').val(),
+        lastname: $('#lastname').val(),
+        cell: parseInt($('#cell').val()),
+        code: parseInt($('#code').val()),
+        email: $('#email').val(),
+        interest: checked,
+        carreer: $('select').val(),
+        assistant: 0,
+        createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+        updatedAt: firebase.firestore.Timestamp.fromDate(new Date())
+    }
+
+    let formAssistant = {
+        idPerson: formData['code'],
+        idMeeting: meetingId,
+        createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+        updatedAt: firebase.firestore.Timestamp.fromDate(new Date())
+    }
+    console.log(formData)
+
+    if ((formData['firstname'] == "") || (formData['lastname'] == "")){
+        errorSaving('No puede dejar los campos de Nombres, Apellidos o Codigo vacios')
+    }
+
+    try {
+        const Assistant = await assistant.where('idMeeting', '==', meetingId).where('idPerson', '==', formData['code']).get()
+        console.log('Asistencias: ',Assistant.docs);
+        const Person = await person.doc($('#code').val()).get()
+        if(Person.exists) name = Person.data().firstname + " " + Person.data().lastname
+        
+        
+        
+        console.log('Person and name: ', Person, name);
+        
+        if (Assistant.docs.length) {
+            errorSaving(`Esta asistencia ya fue registrada al codigo "${formData['code']}" ya fue registrado a nombre de: ${name}`)
+        } else {
+            console.log('La persona existe?: ', Person.exists);
+            
+            (Person.exists) ? await person.doc($('#code').val()).update({ assistant: +1 }) : await person.doc().set(formData)
+            await assistant.doc().set(formAssistant)
+            
+            const meetingData = await meeting.get()
+            noAssistant = meetingData.data().assistant
+            await meeting.update({ assistant: noAssistant+1 })
+        } 
+        $('#firstname').val('')
+        $('#lastname').val('')
+        $('#lastname').val('')
+        $('#cell').val('')
+        $('#code').val('')
+        $('#email').val('')
+        $('select').val('') 
+
+        
+    } catch (error) {
+        console.log(error);
+        errorSaving('Error al guardar en la base de datos, revise su conexion a internet o contacte con el supervisor.')
+    }
+
+    $('#guardar')[0].innerHTML = 'Guardar'
+    getPerson()
+}
