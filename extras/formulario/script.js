3b4479a3e3a8d533b850ddfa30ff6329d682ce94
Ricardo Romo <jricardoromo21@gmail.com>
2019-09-20 00:17:21 -0500

    Formulario registro addedq

--- extras/formulario/script.js
+++ extras/formulario/script.js
@@ -25,41 +25,25 @@ async function registrarAsistente() {
     }
     console.log(formData)
 
-    if ((formData['firstname'] == "") || (formData['lastname'] == "")){
+    if ((formData['firstname'] == "") || (formData['lastname'] == "")) {
         errorSaving('No puede dejar los campos de Nombres, Apellidos o Codigo vacios')
     }
 
     try {
-        const Assistant = await assistant.where('idMeeting', '==', meetingId).where('idPerson', '==', formData['code']).get()
-        console.log('Asistencias: ',Assistant.docs);
-        const Person = await person.doc($('#code').val()).get()
-        if(Person.exists) name = Person.data().firstname + " " + Person.data().lastname
-        
-        
-        
-        console.log('Person and name: ', Person, name);
-        
-        if (Assistant.docs.length) {
-            errorSaving(`Esta asistencia ya fue registrada al codigo "${formData['code']}" ya fue registrado a nombre de: ${name}`)
-        } else {
-            console.log('La persona existe?: ', Person.exists);
-            
-            (Person.exists) ? await person.doc($('#code').val()).update({ assistant: +1 }) : await person.doc().set(formData)
-            await assistant.doc().set(formAssistant)
-            
-            const meetingData = await meeting.get()
-            noAssistant = meetingData.data().assistant
-            await meeting.update({ assistant: noAssistant+1 })
-        } 
+        const meetingData = await meeting.get()
+        await person.doc().set(formData)
+        noAssistant = meetingData.data().assistant
+        await meeting.update({ assistant: noAssistant + 1 })
+
         $('#firstname').val('')
         $('#lastname').val('')
         $('#lastname').val('')
         $('#cell').val('')
         $('#code').val('')
         $('#email').val('')
-        $('select').val('') 
+        $('select').val('')
 
-        
+
     } catch (error) {
         console.log(error);
         errorSaving('Error al guardar en la base de datos, revise su conexion a internet o contacte con el supervisor.')
