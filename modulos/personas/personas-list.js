52fe7ba7790eea2da1d33eaa587015adf3507d47
Ricardo Romo <jricardoromo21@gmail.com>
2019-10-01 12:49:36 -0500

    add eliminar personas

--- modulos/personas/personas-list.js
+++ modulos/personas/personas-list.js
@@ -3,12 +3,15 @@ String.prototype.capitalize = function () {
 };
 
 var options = {
-    valueNames: ['id', 'name', 'code', 'carreer', 'type', 'membership', 'cell', 'email', 'assistant']
+    valueNames: ['id', 'firstname', 'lastname', 'code', 'carreer', 'type', 'membership', 'cell', 'email', 'assistant']
 }
 
 var personList = new List('personList', options);
 
 personList.clear()
+
+var idEditPerson = ''
+
 
 async function getPerson() {
     const Person = await person.get()
@@ -16,12 +19,37 @@ async function getPerson() {
     await Person.forEach(doc => {
         data = doc.data()
         personList.add({
-            id: data.code, name: `${data.firstname} ${data.lastname}`.capitalize(), code: data.code,
-            carreer: data.carreer, type: data.type, membership: data.membership,
-            cell: data.cell, email: data.email, assistant: data.assistant
+            id: data.code,
+            firstname: `${data.firstname}`.capitalize(),
+            lastname: `${data.lastname}`.capitalize(),
+            code: data.code,
+            carreer: `${data.carreer}`.capitalize(),
+            type: data.type,
+            membership: data.membership,
+            cell: data.cell,
+            email: data.email,
+            assistant: data.assistant
         });
 
     })
 }
 
 getPerson()
+
+$("tbody").click(e => {
+    let target = e.target
+    let parent = $(target).parent()
+    let children = parent[0].children
+    console.log(children);
+    console.log(Object.keys(children));
+    var data = {}
+    for (let key = 0; key < children.length; key++) {
+        keyName = children[key].className
+        data[keyName] = children[key].textContent        
+        $(`#${keyName}Edit`).val(children[key].textContent)
+    }
+    idEditPerson = data.id
+    console.log(idEditPerson);
+    $("#editPersonModal").modal('show')
+    console.log($("#editPersonModal"))
+})
